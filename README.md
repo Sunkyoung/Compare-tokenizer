3c1926f60446ffa02f5997024368c27965fb7909
Sunkyoung <line.k0@kaist.ac.kr>
2020-08-06 03:55:26 +0000

    Add experiment of subword segmentation (word piece) tokenization

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
-# Comparison Experiment on Tokenizer
+# Comparative Experiment on Tokenization Methods
 * Environment : Ubuntu 18.04, Python 3.8.3
 * Dataset : Korean Wikipedia
 
-1) Morphology
+1) Morphology (형태소 분석)
 * Package : KoNLPy (Hannanum, Kkma, Komoran, Mecab, Okt), Khaiii
+
+
 * Example) 칙 코리아, 스티브 바이, 제프 벡등의 유명 음악가들과 연주한 바 있다.
     * Hannanum : 칙/코리아/스티브/바/이/,/제프/벡등/의/유명/음악가들/과/연주/하/ㄴ/바/있/다/.
     * Kkma : 칙/코리아/,/스티브/바이/,/저/의/프/벡등/의/유명/음악가/들/과/연주/하/ㄴ/바/있/다/.
@@ -11,13 +13,43 @@
     * Mecab : 칙/코리아/,/스티브/바이/,/제프/벡/등/의/유명/음악가/들/과/연주/한/바/있/다/.
     * Okt : 칙/코리아/, /스티브/바이/,/제프/벡등/의/유명/음악가/들/과/연주/한/바/있다/./\n
     * Khaiii : 칙/코리아/,/스티브/바이/,/제프/벡등/의/유명/음악가/들/과/연주/하/ㄴ/바/있/다/.
+
+
 * Measure Tokenizer Loading Time
     Tokenizer | Hannanum | Kkma | Komoran | Mecab | Okt | Khaiii
     --------- | -------- | ---- | ------- | ----- | --- | ------
     Loading Time (sec) | 0.4185 | 0.0110 | 1.5994 | 0.0005 | 0.0041 | 0.0028  
-=>  **Mecab** < Khaiii < Okt < Kkma < Hannanum < Komoran
+    =>  **Mecab** < Khaiii < Okt < Kkma < Hannanum < Komoran
+
 
 * Measuring Analyzing Time
     * Sampled 10, 100, 1000, 10000 sentences
     ![morph_result](./morph_result.png)  
 => **Mecab** < Khaiii < Komoran < Okt < Hannanum < Kkma 
+
+2) Subword segmentation / Word-piece
+* Package : Huggingface Tokenizers (https://github.com/huggingface/tokenizers)
+
+
+* Example) 칙 코리아, 스티브 바이, 제프 벡등의 유명 음악가들과 연주한 바 있다.
+    * CharBPE : 칙</w>/코/리아</w>/,</w>/스티/브</w>/바이</w>/,</w>/제프</w>/등의</w>/ 유명</w>/음악/가들과</w>/연주/한</w>/바</w>/있다</w>/.</w>
+    * Byte level BPE : *Unicode broken*
+    * BERT Wordpiece : 칙/코/##리아/,/스티/##브/바이/,/제프/[UNK]/유명/음악/##가/##들과/  연주/##한/바/있다/.
+    * Sentencepiece : ▁칙/▁코/리아/, /▁스티/브/▁바/이/,/▁제프/▁등의/▁유명/▁음악/가/  들과/▁연주/한/▁바/▁있다.
+
+
+
+* Measure Tokenizer Loading & Training Time
+    Tokenizer | CharBPE | Byte level BPE | BERT Wordpiece | Sentencepiece
+    --------- | -------- | ---- | ------- | -----
+    Loading Time (sec) | 0.0001 | 0.0069 | 0.0073 | 0.0183 | 
+    Training Time (sec) | 1.1387 | 1.9840 | 1.1990 | 1.4621
+
+
+* Measuring Analyzing Time
+    * Sampled 10, 100, 1000, 10000 sentences
+    ![morph_result](./subword_result.png)  
+=> **SentencePiece** < Bert WordPiece < CharBPE < ByteLevelBPE
+
+reference : 
+[https://iostream.tistory.com/144](https://iostream.tistory.com/144)
---
+++ subword_comparison.py
@@ -0,0 +1,72 @@
+from tokenizers import CharBPETokenizer, ByteLevelBPETokenizer, SentencePieceBPETokenizer, BertWordPieceTokenizer
+import time
+from random import randint
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
+
+# https://github.com/huggingface/tokenizers/tree/master/bindings/python
+# - CharBPETokenizer: The original BPE
+# - ByteLevelBPETokenizer: The byte level version of the BPE
+# - SentencePieceBPETokenizer: A BPE implementation compatible with the one used by SentencePiece
+# - BertWordPieceTokenizer
+
+f = open('dataset/sample-context.txt','r')
+lines = f.readlines()
+print('total lines : ',len(lines))
+n = randint(0, len(lines))
+
+def tokenizer_train(tokenizer):
+    restart = time.time()
+    tokenizer.train(["Compare-tokenizer/dataset/sample-context.txt"], vocab_size=16000)
+    print('Training Time : ', time.time()-restart)
+    return tokenizer
+
+def tokenizer_output(tokenizer, name):
+    y = []
+    print('Original Sentence : ',lines[n])
+    output = tokenizer.encode(lines[n])
+    print('Random Analyze Result : ', output.tokens)
+
+    start = time.time()
+    output = tokenizer.encode(''.join(lines[:11]))
+    y.append(time.time()-start)
+    start = time.time()
+    output = tokenizer.encode(''.join(lines[:101]))
+    y.append(time.time()-start)
+    start = time.time()
+    output = tokenizer.encode(''.join(lines[:1001]))
+    y.append(time.time()-start)
+    start = time.time()
+    output = tokenizer.encode(''.join(lines))
+    y.append(time.time()-start)
+
+    print('Measured time : ', y)
+    draw_plot(y, name)
+
+def draw_plot(y, name):
+    x = [10, 100, 1000, 10000]
+    plt.plot(x, y, label=name)
+
+# Initialize a tokenizer
+start = time.time()
+tokenizer = CharBPETokenizer()
+print('CharBPE Loading Time : ', time.time()-start)
+tokenizer_output(tokenizer_train(tokenizer), 'CharBPE')
+
+start = time.time()
+tokenizer = SentencePieceBPETokenizer()
+print('SentencePiece Loading Time : ', time.time()-start)
+tokenizer_output(tokenizer_train(tokenizer), 'SentencePiece')
+
+start = time.time()
+tokenizer = ByteLevelBPETokenizer()
+print('ByteLevelBPE Loading Time : ', time.time()-start)
+tokenizer_output(tokenizer_train(tokenizer), 'ByteLevelBPE')
+
+start = time.time()
+tokenizer = BertWordPieceTokenizer(lowercase=False)
+print('BertWordPiece Loading Time : ', time.time()-start)
+tokenizer_output(tokenizer_train(tokenizer), 'BertWordPiece')
+
+plt.legend()
+plt.savefig('subword_result.png')
